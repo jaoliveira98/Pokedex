dbb0d0a35e38bf8227b0c95fcb258d34389e1a5d
Joao Oliveira <jaoliveira98@gmail.com>
2023-07-13 15:04:08 +0100

    Pokemon Cards Component

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const PokemonCard = () => {
+	return (
+		<div>PokemonCard</div>
+		// this will be the component that will be called on PokemonCards. It is just the UI
+	);
+};
+
+export default PokemonCard;
---
+++ src/components/PokemonCards.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const data = [
+	{ id: 1, name: "Bulbasaur", type: ["Grass", "Poisen"] },
+	{ id: 2, name: "Ivysaur", type: ["Grass", "Poisen"] },
+	{ id: 3, name: "Venusaur", type: ["Grass", "Poisen"] },
+	{ id: 4, name: "Charmander", type: ["Fire"] },
+];
+
+const PokemonCards = () => {
+	return (
+		<>
+			{data.map((item) => (
+				<div key={item.id}>
+					<div>
+						<h1>{item.name}</h1>
+						<span>{item.id}</span>
+					</div>
+				</div>
+			))}
+		</>
+	);
+};
+
+export default PokemonCards;
