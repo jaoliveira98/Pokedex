6b7bb0d7332c2ca50e56c7f1dc3d77e20060f150
Joao Oliveira <jaoliveira98@gmail.com>
2023-07-13 15:03:34 +0100

    Routing

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />,
+		errorElement: <ErrorPage />,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
