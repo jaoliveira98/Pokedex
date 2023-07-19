1a546fd977c95ae3a4d8996782bd2373fca3eff6
Joao Oliveira <jaoliveira98@gmail.com>
2023-07-19 14:28:53 +0100

    usePagination Hook

---
+++ src/hooks/usePagination.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const POKEMONS_PER_PAGE = 24;
+
+const usePagination = (data) => {
+	const [currentPage, setCurrentPage] = useState(1);
+
+	// Pages = items per page
+	const numPages = Math.ceil(data.length / POKEMONS_PER_PAGE);
+
+	// Previous page
+	const previousPage = () => {
+		// Go back if not on first page
+		if (currentPage !== 1) {
+			setCurrentPage(currentPage - 1);
+		}
+	};
+
+	// Next page
+	const nextPage = () => {
+		// Go forward if not on last page
+		if (currentPage !== numPages) {
+			setCurrentPage(currentPage + 1);
+		}
+	};
+
+	// Calculate the data to display on the current page
+	const currentPageData = data.slice(
+		(currentPage - 1) * POKEMONS_PER_PAGE,
+		currentPage * POKEMONS_PER_PAGE
+	);
+
+	return {
+		currentPage,
+		numPages,
+		previousPage,
+		nextPage,
+		currentPageData,
+		setCurrentPage,
+	};
+};
+
+export default usePagination;
