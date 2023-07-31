7669926deaad67884d1118634468e42db1e251f4
Joao Oliveira <jaoliveira98@gmail.com>
2023-07-31 14:47:53 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import React from "react";
+import App from './App';
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./pages/ErrorPage";
-import Homepage from "./pages/Homepage";
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Homepage />,
-		errorElement: <ErrorPage />,
-	},
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<App/>
 	</React.StrictMode>
 );
